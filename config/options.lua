537c71786b0681e2b9cd822a59b7e6c24fa88099
Motoki Omamiuda <omamiuda1011@gmail.com>
2026-02-20 16:28:50 +0900

    update: lua files

---
+++ config/options.lua
@@ -0,0 +1,7 @@
+vim.opt.number = true          -- 行番号を表示
+vim.opt.relativenumber = false -- カーソル行からの相対行番号を表示
+vim.opt.expandtab = true       -- Tab key入力をタブ文字でなくスペースに変換
+vim.opt.shiftwidth = 2         -- インデント時の幅をスペース個数設定
+vim.opt.tabstop = 2            -- タブ文字を画面上でスペース個数設定
+vim.opt.ignorecase = true      -- 検索時に大文字・小文字を区別しない
+vim.opt.smartcase = false      -- 検索語に大文字が含まれる場合のみ大小区別する
