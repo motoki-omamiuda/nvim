d4d18a5e72538dccbfb144a6cccecb70e0be1315
Motoki Omamiuda <omamiuda1011@gmail.com>
2026-03-02 09:50:52 +0900

    update: 2026/03/02 09:50

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,33 +1,105 @@
 require("lazy").setup({
- 
-  -- 1. サイドバー (nvim-tree)
+
+  -- treesitter
+  -- description: display code color
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      ensure_installed = { "python", "lua", "vim", "vimdoc" },
+      highlight = { 
+        enable = true, 
+      },
+      indent = { enable = true },
+    },
+  },
+
+  -- lspconfig
+  -- description: activate ruff and pyright
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    opts = {
+      servers = {
+        pyright = {},
+        ruff = {},
+      },
+    },
+    config = function(_, opts)
+      for server, config in pairs(opts.servers) do
+        vim.lsp.config(server, config)
+        vim.lsp.enable(server)
+      end
+    end,
+  },
+
+  -- mason
+  -- description: setup for  LSP server
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    opts = {
+      ensure_installed = { "pyright", "ruff" },
+    },
+  },
+
+  -- git sign
+  -- description: display git diffs
+  {
+    'lewis6991/gitsigns.nvim',
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      signs = {
+        add          = { text = 'A' },
+        change       = { text = 'U' },
+        delete       = { text = 'D' },
+        topdelete    = { text = 'D' },
+        changedelete = { text = 'D' },
+      },
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 500,
+      },
+    },
+  },
+
+
+  -- nvim-tree
+  -- discription: display file explore
   {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("nvim-tree").setup({
-        view = { width = 30, side = "left" },
-        git = { enable = true, ignore = false, timeout = 500 },
+        view = {
+          width = 30,
+          side = "left"
+        },
+        git = {
+          enable = true,
+          ignore = false,
+          timeout = 500
+        },
       })
     end,
   },
-
-  -- 2. 上部タブ (bufferline)
+  
+  -- bufferline
+  -- description: display upper tab
   {
     "akinsho/bufferline.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("bufferline").setup({})
-    end,
-  },
-
-  -- 3. カラースキーム（見た目をかっこよくするなら）
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+    opts = {
+      options = {
+        mode = "buffers",
+        numbers = "ordinal",
+        always_show_bufferline = true,
+      },
+    },
+  }, 
 })
